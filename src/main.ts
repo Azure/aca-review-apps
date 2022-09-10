5fdb4a4e0e9376295f54d9f3c4db8ddb02dce58d
ry0y4n <rusk.ryoy4n@gmail.com>
2022-09-10 19:30:00 +0900

    Minimally fixed errors where values were not passed

--- src/main.ts
+++ src/main.ts
@@ -11,14 +11,12 @@ async function main() {
 
 // Please refer to this sample code
 // https://github.com/Azure/azure-sdk-for-js/blob/32c07776aa91c302fb2c90ba65e3bb4668b5a792/sdk/appcontainers/arm-appcontainers/samples-dev/containerAppsCreateOrUpdateSample.ts
-
   try {
     // Set user agent variable.
     let usrAgentRepo = crypto.createHash('sha256').update(`${process.env.GITHUB_REPOSITORY}`).digest('hex');
     let actionName = 'DeployAzureContainerApp';
     let userAgentString = (!!prefix ? `${prefix}+` : '') + `GITHUBACTIONS_${actionName}_${usrAgentRepo}`;
     core.exportVariable('AZURE_HTTP_USER_AGENT', userAgentString);
-
     var taskParams = TaskParameters.getTaskParams();
     let credential: TokenCredential = new DefaultAzureCredential()
 
@@ -29,32 +27,60 @@ async function main() {
     const client = new ContainerAppsAPIClient(credential, taskParams.subscriptionId);
 
     // TBD: Remove key when there is key without value
-    const daprConfig = {
+    const daprConfig: {
+      appPort?: number,
+      appProtocol?: string,
+      enabled: boolean
+    } = {
       appPort: taskParams.daprAppPort, 
       appProtocol: taskParams.daprAppProtocol, 
       enabled: taskParams.daprEnabled
     };
+    if (isNaN(taskParams.daprAppPort)) {
+      delete daprConfig.appPort
+    };
+    if (taskParams.daprAppProtocol == "") {
+      delete daprConfig.appProtocol
+    };
 
     // TBD: Remove key when there is key without value
-    const ingresConfig = {
+    const ingresConfig: {
+      external: boolean,
+      targetPort?: number,
+      traffic?: any[],
+      customDomains?: any[]
+    } = {
       external: taskParams.ingressExternal, 
       targetPort: taskParams.ingressTargetPort, 
       // traffic: taskParams.ingressTraffic, 
       // customDomains: taskParams.ingressCustomDomains
-    } 
+    };
+    if (taskParams.ingressTraffic.length == 0) {
+      delete ingresConfig.traffic
+    };
 
     let scaleRules = taskParams.scaleRules
     // TBD: Remove key when there is key without value
-    const scaleConfig = {
+    const scaleConfig: {
+      maxReplicas: number,
+      minReplicas: number,
+      rules: any[]
+    } = {
       maxReplicas: taskParams.scaleMaxReplicas, 
       minReplicas: taskParams.scaleMinReplicas, 
       rules: scaleRules 
-    }
+    };
 
-    let networkConfig = {
-      "dapr": daprConfig,
-      "ingress": ingresConfig
-    }
+    let networkConfig: {
+      dapr: object,
+      ingress?: object
+    } = {
+      dapr: daprConfig,
+      ingress: ingresConfig
+    };
+    if (taskParams.ingressExternal == false) {
+      delete networkConfig.ingress
+    };
 
     // TBD: Find a way to get a value instead of json
     const containersConfig = taskParams.containersConfig
