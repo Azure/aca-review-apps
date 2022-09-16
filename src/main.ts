d906a017888a2e2cd09276574af3a29b5fee1a03
ry0y4n <rusk.ryoy4n@gmail.com>
2022-09-16 09:16:50 +0900

    get commit hash directly from typescript

--- src/main.ts
+++ src/main.ts
@@ -41,7 +41,7 @@ async function main() {
       }
     });
     traffics.push({
-      revisionName: `${taskParams.containerAppName}--${taskParams.commitHash}`,
+      revisionName: `${taskParams.containerAppName}--${process.env.GITHUB_SHA}`,
       weight: 0,
       latestRevision: false
     })
@@ -105,7 +105,7 @@ async function main() {
       template: {
         containers: containerConfig,
         scale: scaleConfig,
-        revisionSuffix: taskParams.commitHash
+        revisionSuffix: process.env.GITHUB_SHA
       }
     };
 
--- src/taskparameters.ts
+++ src/taskparameters.ts
@@ -10,7 +10,6 @@ export class TaskParameters {
     private _resourceGroup: string;
     private _containerAppName: string;
     private _imageName: string;
-    private _commitHash: string;
     private _subscriptionId: string;
 
     // Optional Dapr parameters
@@ -37,7 +36,6 @@ export class TaskParameters {
         this._resourceGroup = core.getInput('resource-group', { required: true });
         this._containerAppName = core.getInput('name', { required: true });
         this._imageName = core.getInput('image', { required: true });
-        this._commitHash = core.getInput('commit-hash', { required: true });
 
         // Optional Dapr parameters
         this._daprAppPort = parseInt(core.getInput('dapr-app-port', { required: false }));
@@ -77,10 +75,6 @@ export class TaskParameters {
 
     public get imageName() {
         return this._imageName;
-    }
-
-    public get commitHash() {
-        return this._commitHash;
     }
 
     public get subscriptionId() {
